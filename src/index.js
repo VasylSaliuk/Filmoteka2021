547e93f2deb5cc3878ade81f0ae7edc4fd2fde2e
Vasyl Saliuk <67233665+VasylSaliuk@users.noreply.github.com>
2021-01-31 12:38:06 +0200

    add slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
-import './sass/main.scss'
+import './sass/main.scss';
 import './js/btnUp.js';
 import './js/1iniitalHomePage';
 import './js/2searchAndPlaginationHomePage.js';
 import './js/3navigation.js';
 import './js/4filmDeteilsPage.js';
 import './js/5libraryPage.js';
+import './js/slider.js';
